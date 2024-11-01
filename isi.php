449471020c132e71260278dea7327182e3f2f162
alief08 <alifadjie001@gmail.com>
2024-11-02 00:19:25 +0800

    Add files via upload

---
+++ isi.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+    <link href="style.css" rel="stylesheet"> 
+    <title>Happy Birthday</title>
+  </head>
+  <body>
+  <audio autoplay="2s"><source src="song/Sempurna.mp3" type="audio/mp3"/></audio>
+  <section class="latar"><div class="container"><div class="image container d-flex align-items-center justify-content-center fs-1 text-white flex-column" style="width: 17rem;">
+  <img src="birthday.png" class="card-img-top mt-3" alt="...">
+  <p class="p mt-2">"Selamat ulang tahun Tashaa! Semoga tahun baru dalam hidupmu ini membawa kesempatan, kebahagiaan, dan pencapaian yang tak terduga. Teruslah berjalan dengan penuh percaya diri, Semangat teruss yaa Tashaaa, tetap bareng yuda, Sekali lagi Selamatt Ulang Tahun Cantikk"<a href="index.html" class="btn btn-light mt-2 ms-3">Pencet Lagi!!!</a>
+  
+  </p>
+    
+</div>
+  
+  
+</div></section>
+  
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  
+  </body>
+</html>
